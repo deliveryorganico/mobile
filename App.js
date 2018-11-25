257b7dd46ff5e21cf1f7418885acdb0b77c31a84
Facundo <facu1910@gmail.com>
2018-11-25 15:19:10 -0300

    se crean nuevos js por screen

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, Navigator } from 'react-native';
 import Login from './src/components/Login/LoginView';
 import { createStackNavigator, createAppContainer, createBottomTabNavigator, createTabNavigator, TabBarBottom } from "react-navigation";
-import {Button,BottomNavigation} from 'react-native-material-ui';
+import {Button,BottomNavigation,ListItem} from 'react-native-material-ui';
 import { Ionicons } from '@expo/vector-icons';
+import AppNavigator from './screens/AppNavigator';
+
 
 
 // export default class App extends React.Component {
@@ -14,10 +16,7 @@ import { Ionicons } from '@expo/vector-icons';
 //       );
 //     }
 //   }
-class LoginScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Login',
-  };
+export  class LoginScreen extends React.Component {
   render() {
     return (
       <Login/>
@@ -29,15 +28,19 @@ class LoginScreen extends React.Component {
     static navigationOptions = {
       title: 'Home',
       headerLeft: null,
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: '#FFC107',
+      },
     };
     render() {
       return (
         <View>
-        <Text>Home</Text>
         </View>
         )
       }
     }
+
     const RootStack = createStackNavigator(
       {
         Login: LoginScreen,
@@ -52,6 +55,7 @@ class LoginScreen extends React.Component {
       export default class App extends React.Component {
         render() {
           return <AppContainer />;
+          
         }
-      }
+     }
       
