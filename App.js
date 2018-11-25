16a72ec961f43978645b2e03dcce40d97a6b8c18
Facundo <facubarafani@hotmail.com>
2018-11-25 14:14:58 -0300

    navigation agregado

--- App.js
+++ App.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import { StyleSheet, Text, View, Navigator } from 'react-native';
 import Login from './src/components/Login/LoginView';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator, createTabNavigator, TabBarBottom } from "react-navigation";
+import {Button,BottomNavigation} from 'react-native-material-ui';
+import { Ionicons } from '@expo/vector-icons';
 
 
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Login/>
 
-export default class App extends React.Component {
+//       );
+//     }
+//   }
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+  };
   render() {
     return (
-          <Login/>
-
-    );
+      <Login/>
+      );
+    }
   }
-}
-
-
+  
+  class HomeScreen extends React.Component {
+    static navigationOptions = {
+      title: 'Home',
+      headerLeft: null,
+    };
+    render() {
+      return (
+        <View>
+        <Text>Home</Text>
+        </View>
+        )
+      }
+    }
+    const RootStack = createStackNavigator(
+      {
+        Login: LoginScreen,
+        Home: HomeScreen,
+      },
+      {
+        initialRouteName: 'Home',
+      }
+      );
+      
+      const AppContainer = createAppContainer(RootStack);
+      export default class App extends React.Component {
+        render() {
+          return <AppContainer />;
+        }
+      }
+      
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -6,81 +6,82 @@ export default class LoginForm extends React.Component {
     render() {
         return (
             <View>
-                <Text style={styles.texto}>Ingrese su nombre de usario</Text>
-                <TextInput
-                    placeholder='Nombre de Usuario'
-                    style={styles.input}
-                />
-                <Text style={styles.texto2}>Ingrese su contraseña</Text>
-                <TextInput
-                    placeholder='Contraseña'
-                    style={styles.input}
-                />
-                <View style={styles.btn}>
-                    <Button onPress={(this.onLogin.bind(this))} raised primary text="LogIn"/>
-                    <View style={styles.btn2}>
-                        <Button onPress={(this.onLogin.bind(this))} raised primary text="Registrarse"/>
-                    </View>
-                </View>
-
-
+            <Text style={styles.texto}>Ingrese su nombre de usuario</Text>
+            <TextInput
+            placeholder='Nombre de Usuario'
+            style={styles.input}
+            />
+            <Text style={styles.texto2}>Ingrese su contraseña</Text>
+            <TextInput
+            placeholder='Contraseña'
+            style={styles.input}
+            />
+            <View style={styles.btn}>
+            <Button onPress={(this.onLogin.bind(this))} raised primary text="Login"/>
+            <View style={styles.btn2}>
+            <Button onPress={(this.onLogin.bind(this))} raised primary text="Registrarse"/>
             </View>
-        )
-    }
-
-    onLogin() {
-        Alert.alert(
-            'Acceso',
-            'Te has logueado',
-            [
-                {
-                    text: 'Aceptar',
-                    onPress: (this.aceptar.bind(this))
-                },
-                {
-                    text: 'Cancelar',
-                    onPress: (this.cancelar.bind(this))
-                }
-            ]
-        )
-    }
-
-    aceptar() {
-        console.log('sucess')
-    }
-
-    cancelar() {
-        console.log('denied')
-    }
-}
-
-
-const styles = StyleSheet.create({
-    input: {
-        height: 40,
-        backgroundColor: '#9E9E9E',
-        marginBottom: 20,
-        color: 'black',
-        paddingHorizontal: 10
-    },
-    texto: {
-        marginBottom: 5,
-        marginTop: 5,
-        marginLeft: 68,
-        fontSize: 20,
-        color: 'black'
-    },
-    texto2: {
-        marginBottom: 5,
-        marginLeft: 90,
-        fontSize: 20
-    },
-    btn: {
-        alignItems: 'center',
-        flexDirection: "row",
-        padding: 10
-    },
-    btn2: {
-        marginLeft: 10
-    }
-})
+            </View>
+            
+            
+            </View>
+            )
+        }
+        
+        onLogin() {
+            Alert.alert(
+                'Acceso',
+                'Te has logueado',
+                [
+                    {
+                        text: 'Aceptar',
+                        onPress: (this.aceptar.bind(this))
+                    },
+                    {
+                        text: 'Cancelar',
+                        onPress: (this.cancelar.bind(this))
+                    }
+                ]
+                )
+            }
+            
+            aceptar() {
+                console.log('sucess')
+            }
+            
+            cancelar() {
+                console.log('denied')
+            }
+        }
+        
+        
+        const styles = StyleSheet.create({
+            input: {
+                height: 40,
+                backgroundColor: '#9E9E9E',
+                marginBottom: 20,
+                color: 'black',
+                paddingHorizontal: 10
+            },
+            texto: {
+                marginBottom: 5,
+                marginTop: 5,
+                marginLeft: 68,
+                fontSize: 20,
+                color: 'black'
+            },
+            texto2: {
+                marginBottom: 5,
+                marginLeft: 90,
+                fontSize: 20
+            },
+            btn: {
+                alignItems: 'center',
+                flexDirection: "row",
+                padding: 10
+            },
+            btn2: {
+                marginLeft: 10
+            }
+        })
+        
