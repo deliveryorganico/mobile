e836a8f07f6a54c72ac20d0eb6ca547e4e1d0d55
fedevargas110 <fedevargas110@gmail.com>
2018-11-27 10:56:16 -0300

    Profile.js creado

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Profile from './components/Profile/Profile';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Profile/>
       </View>
     );
   }
