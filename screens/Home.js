a2b388dacece98764c267a5b98c3507dd79a0b1a
Facundo <facu1910@gmail.com>
2018-11-27 17:17:54 -0300

    Header agregado a Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View,Image } from 'react-native'
-import { Card, ListItem, Button } from 'react-native-elements'
+import { Header, Card, ListItem, Button } from 'react-native-elements'
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 export class HomeScreen extends React.Component {
@@ -9,20 +9,24 @@ export class HomeScreen extends React.Component {
     };
     render() {
         return (
-            <View>
-            <Card
-            title='Banana'
-            image={require('../images/banana.jpg')}>
-            <Text style={{marginBottom: 10}}>
-            Probando
-            </Text>
-            <Button
-            icon={{name: 'shopping-cart'}}
-            backgroundColor='#03A9F4'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='COMPRAR' />
-            </Card>
-            </View>
+            <React.Fragment>
+                <Header
+                    centerComponent={{ text: 'HOME', style: { color: '#fff' } }}
+                    outerContainerStyles={{ backgroundColor: '#FFC107' }}
+                />
+                <Card
+                    title='Banana'
+                    image={require('../images/banana.jpg')}>
+                    <Text style={{marginBottom: 10}}>
+                    Probando
+                    </Text>
+                    <Button
+                    icon={{name: 'shopping-cart'}}
+                    backgroundColor='#03A9F4'
+                    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                    title='COMPRAR' />
+                </Card>
+            </React.Fragment>
             );
         }
     }
