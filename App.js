1c280b2f6acf2201bb7170ff245a626d268c4132
Facundo <facu1910@gmail.com>
2018-11-27 15:54:39 -0300

    react-native-elements instalado // navtab agregado

--- App.js
+++ App.js
@@ -1,61 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Navigator } from 'react-native';
-import Login from './src/components/Login/LoginView';
-import { createStackNavigator, createAppContainer, createBottomTabNavigator, createTabNavigator, TabBarBottom } from "react-navigation";
-import {Button,BottomNavigation,ListItem} from 'react-native-material-ui';
-import { Ionicons } from '@expo/vector-icons';
-import AppNavigator from './screens/AppNavigator';
-
-
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <Login/>
-
-//       );
-//     }
-//   }
-export  class LoginScreen extends React.Component {
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import Tabs from './screens/AppNavigator';
+export default class App extends React.Component {
   render() {
     return (
-      <Login/>
-      );
+      <Tabs/>
+      )
     }
   }
-  
-  class HomeScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Home',
-      headerLeft: null,
-      headerTintColor: 'white',
-      headerStyle: {
-        backgroundColor: '#FFC107',
-      },
-    };
-    render() {
-      return (
-        <View>
-        </View>
-        )
-      }
-    }
-
-    const RootStack = createStackNavigator(
-      {
-        Login: LoginScreen,
-        Home: HomeScreen,
-      },
-      {
-        initialRouteName: 'Home',
-      }
-      );
-      
-      const AppContainer = createAppContainer(RootStack);
-      export default class App extends React.Component {
-        render() {
-          return <AppContainer />;
-          
-        }
-     }
-      
