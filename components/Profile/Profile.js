04799afa34d107108e673f53494a1635338453a1
fedevargas110 <fedevargas110@gmail.com>
2018-11-29 02:13:49 -0300

    Profile avanzado

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from 'react-native-elements'
+import { ListItem } from 'react-native-elements'
+
+const questions = [
+    {
+      title: 'User',
+      icon: 'person'
+    },
+    {
+      title: 'Manage payment methods',
+      icon: 'payment'
+    },
+    {
+        title: 'Manage address',
+        icon: 'place'
+    },
+    {
+        title: 'Contact Us',
+        icon: 'inbox'
+    },
+    {
+        title: 'Add',
+        icon: 'add'
+    }
+  ]
 
 export default class App extends React.Component {
 	render() {
@@ -9,9 +33,28 @@ export default class App extends React.Component {
                 <Header
                     leftComponent={{icon: 'face', color: '#fff' } }
                     centerComponent={{ text: 'PROFILE', style: { color: '#fff' } }}
+                    rightComponent={{icon: 'more-vert', color:'#fff'}}
                     outerContainerStyles={{ backgroundColor: '#FFC107' }}
                 />
- 			</View>
+                {questions.map( (questions, index) =>(
+                    <ListItem 
+                    key={index} 
+                    leftIcon={{name: questions.icon}}
+                    title={questions.title}/>
+                ) )}
+            </View>
 		);
 	}
 }
+//<ListItem 
+//                    title='User'
+//                    leftIcon={{name:'person'}}/>
+//                <ListItem
+//                    title= 'Manage payment methods'
+//                    leftIcon={{name:'payment'}} />
+//                <ListItem
+//                    title='Manage address'
+//                    leftIcon={{name:'place'}}/>
+//                <ListItem
+//                title= 'Contact Us'
+//                leftIcon={{name:'inbox'}} />
