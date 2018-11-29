1cda0326c25b94d90ae2617148e9c6db550961f8
fedevargas110 <fedevargas110@gmail.com>
2018-11-29 01:09:19 -0300

    toolbar modificado

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Profile from './components/Profile/Profile';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <Profile/>
-    );
+      )
+    }
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { Toolbar } from 'react-native-material-ui';
+import { Header } from 'react-native-elements'
 
 export default class App extends React.Component {
 	render() {
 		return(
-			<View>
-        <Toolbar
-        leftElement="menu"
-        centerElement="Profile"
-        rightElement={{
-            menu: {
-                icon: "more-vert",
-                labels: ["item 1", "item 2"]
-            }
-        }}
-        onRightElementPress={ (label) => { console.log(label) }}
-      />
+			<View style={{flex: 1, backgroundColor: '#ddd'}}>
+                <Header
+                    leftComponent={{icon: 'face', color: '#fff' } }
+                    centerComponent={{ text: 'PROFILE', style: { color: '#fff' } }}
+                    outerContainerStyles={{ backgroundColor: '#FFC107' }}
+                />
  			</View>
 		);
 	}
